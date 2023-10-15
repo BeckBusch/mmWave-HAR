b58730d6f37e9f697fc195afc06761e7e4e32d81
AkuinAshura <samuel.louis.mason@gmail.com>
2023-10-15 23:28:35 +1300

    Added renamer, commenting
    Fully commented data_augmenter. Renaming script for specifically renaming activity data collected on researchers (not general purpose).

--- Main/data_augmenter.py
+++ Main/data_augmenter.py
@@ -1,24 +1,21 @@
 import numpy as np
-import pandas as pd
 import csv
 
-# We want to augment the data by producing synthetic samples, in order to increase the data that we can train on.
+# We want to augment the data by resampling, in order to increase the data that we can train on.
 
 # For each activity of 6 seconds in length, we want to produce samples of 2, 3, 4, 5 and 6s, with 5, 4, 3, 2, 1 samples respectively.
 # This totals 15 samples per sample, so we can get a x15 increase in the size of the dataset.
 
-# Defines
 FRAMES_PER_SECOND = 25 # From mmWave Studio.
 SAMPLE_LENGTH = 6 # Sample length in seconds.
-IMAGE_PIXELS = 77 # Pixels in a single image.
+IMAGE_PIXELS = 77 # Pixels in a single reduced image.
 
-path = 'C:\\Users\\Samuel Mason\\Documents\\GitHub\\mmWave-HAR\\Main\\reduced_data2.csv' # Path to the csv file with all of the activity data.
-# The path being used here is for the reduced data i.e. the data that has had vertical (and potentially horizontal) image compression done.
+path = 'C:\\Users\\Samuel Mason\\Documents\\GitHub\\mmWave-HAR\\Main\\reduced_data.csv' # Path to the csv file with all of the activity data.
+# The path being used here is for the reduced data i.e. the data that has had image compression done.
 print(f"Path being used is: {path}")
 
 # Read in the activity data, create a dataframe for it with the rows and columns transposed for optimisation.
 activity_data = open(path).readlines()
-del activity_data[0]
 print("Activity data has been read in")
 
 # Total number of activities recorded.
@@ -30,11 +27,7 @@ for i in range(activity_count):
     temp[-1] = temp[-1].split('\n')[0] # Remove the newline character at the end.
     activity_name = temp[0] 
     temp = temp[1:] # Remove the activity name (it is a string, not complex dtype, so separate it).
-    # cmplx = []
-    # for s in temp:
-    #     cmplx.append(s)
     temp.insert(0, activity_name)
-    # print(temp)
     all_data.append(temp)
 
 all_samples = []
@@ -43,37 +36,30 @@ for p in range(activity_count):
     this_sample = all_data[p] # Get a new activity sample to augment.
     class_name = this_sample[0]
     this_sample = this_sample[1:]
-    # print(this_sample)
-    # print(len(this_sample))
-    # print('\n\n')
-    for i in range(SAMPLE_LENGTH):
-        for j in range(i):
+
+    for i in range(SAMPLE_LENGTH - 1):
+        n = i + 1
+        for j in range(n):
             # The above loops produce an inner loop of 1, 2, 3, 4, 5 loops.
             if j == 0:
-                start_seconds = 0 # For the maximum length we can't clip.
+                start_seconds = 0 # For the maximum length of 6s we can't clip because we need the entire sample.
             else:
                 start_seconds = np.random.uniform(0.0, 1.0) + j - 1 # Generate a uniformly distributed random number between 0 and 1 to be used to offset the subsample.
-            # print(start_seconds)
             duration = SAMPLE_LENGTH - i # Point, in seconds, where we end the subsample clipping.
             start_ptr = round(FRAMES_PER_SECOND * start_seconds) # Pointer to the starting frame. 
-            #print(start_ptr)
             end_ptr = round(FRAMES_PER_SECOND * duration + start_ptr) # Pointer to the ending frame.
-            #print(end_ptr)
             new_sample = this_sample[start_ptr * IMAGE_PIXELS : end_ptr * IMAGE_PIXELS]
-            # print(f'START {start_ptr * IMAGE_PIXELS} END {end_ptr * IMAGE_PIXELS}\n')
             new_sample = list(new_sample)
-            # print(this_sample[0])
-            new_sample.insert(0, class_name)
+            new_sample.insert(0, class_name) # Attach the class name again.
             for q in range(IMAGE_PIXELS * FRAMES_PER_SECOND * (SAMPLE_LENGTH - duration)):
                 new_sample.append(0) # Append zeros to fill the rest of the row - this way the rows will have uniform length.
-            # print(new_sample[2])
-            # Append the array containing the newly generated sample to the csv file.
 
+            # Append the array containing the newly generated sample to the csv file.
             all_samples.append(new_sample)
 
-with open('augmented_data.csv', 'w', newline='') as file:
-# Step 4: Using csv.writer to write the list to the CSV file
-    writer = csv.writer(file)
-    writer.writerows(all_samples) # Use writerow for single list
-
 # Once we have done this for every data sample and written to the output csv, we are done.
+with open('augmented_data.csv', 'w', newline='') as file:
+# Using csv.writer to write the list to the CSV file.
+    writer = csv.writer(file)
+    writer.writerows(all_samples)
+
---
+++ Main/renamer.py
@@ -0,0 +1,80 @@
+import csv
+
+# This file is for renaming activity data to be consistent with the naming scheme.
+
+FRAMES_PER_SECOND = 25 # From mmWave Studio.
+SAMPLE_LENGTH = 6 # Sample length in seconds.
+IMAGE_PIXELS = 77 # Pixels in a single reduced image.
+
+path = 'C:\\Users\\Samuel Mason\\Documents\\GitHub\\mmWave-HAR\\Main\\to_rename_data.csv' # Path to the csv file with all of the activity data.
+print(f"Path being used is: {path}")
+
+# Read in the activity data, create a dataframe for it with the rows and columns transposed for optimisation.
+activity_data = open(path).readlines()
+print("Activity data has been read in")
+
+# Total number of activities recorded.
+activity_count = len(activity_data)
+all_data = []
+
+for i in range(activity_count):
+    temp = activity_data[i].split(',')
+    temp[-1] = temp[-1].split('\n')[0] # Remove the newline character at the end.
+    activity_name = temp[0] 
+    temp = temp[1:] # Remove the activity name (it is a string, not complex dtype, so separate it).
+    temp.insert(0, activity_name)
+    all_data.append(temp)
+
+all_samples = []
+
+for p in range(activity_count):
+    this_sample = all_data[p] # Get an activity sample to rename.
+    class_name = this_sample[0]
+    this_sample = this_sample[1:]
+
+    this_class = class_name.split('_')[0] # Get the class name (old).
+    this_class_index = class_name.split('_')[1] # Get activity index belonging to participant for this class.
+
+    if this_class == "blank":
+        new_class_name = "empty" + "_00" + "_6s_" + this_class_index # Empty doesn't belong to any participant number
+    elif this_class == "kevinclapping":
+        new_class_name = "clapping" + "_01" + "_6s_" + this_class_index
+    elif this_class == "KevinJacks":
+        new_class_name = "jumpingjacks" + "_01" + "_6s_" + this_class_index
+    elif this_class == "kevinStanding":
+        new_class_name = "standing" + "_01" + "_6s_" + this_class_index
+    elif this_class == "kevinWalking":
+        new_class_name = "walking" + "_01" + "_6s_" + this_class_index
+    elif this_class == "kevinWaving":
+        new_class_name = "waving" + "_01" + "_6s_" + this_class_index
+    elif this_class == "samClapping":
+        new_class_name = "clapping" + "_02" + "_6s_" + this_class_index
+    elif this_class == "SamJacks":
+        new_class_name = "jumpingjacks" + "_02" + "_6s_" + this_class_index
+    elif this_class == "samStanding":
+        new_class_name = "standing" + "_02" + "_6s_" + this_class_index
+    elif this_class == "SamWalking":
+        new_class_name = "walking" + "_02" + "_6s_" + this_class_index
+    elif this_class == "samWaving":
+        new_class_name = "waving" + "_02" + "_6s_" + this_class_index
+    elif this_class == "samWalking":
+        new_class_name = "walking" + "_02" + "_6s_" + this_class_index
+    elif this_class == "beckClapping":
+        new_class_name = "clapping" + "_03" + "_6s_" + this_class_index
+    elif this_class == "BeckJacks":
+        new_class_name = "jumpingjacks" + "_03" + "_6s_" + this_class_index
+    elif this_class == "beckStanding":
+        new_class_name = "standing" + "_03" + "_6s_" + this_class_index
+    elif this_class == "beckWalking":
+        new_class_name = "walking" + "_03" + "_6s_" + this_class_index
+    elif this_class == "beckWaving":
+        new_class_name = "waving" + "_03" + "_6s_" + this_class_index
+
+    this_sample.insert(0, new_class_name) # Attach the new class name.
+    all_samples.append(this_sample)
+
+# Once we have done this for every data sample and written to the output csv, we are done.
+with open('reduced_data.csv', 'w', newline='') as file:
+# Using csv.writer to write the list to the CSV file.
+    writer = csv.writer(file)
+    writer.writerows(all_samples)
